7661a6e7bb95b892dabf6ef067585db2f1c7da3b
Kaio Meireles <dickkaiosilva@gmail.com>
2025-09-01 21:15:49 -0300

    Classe 04 e teste

---
+++ src/exercicio1oo/classes/TesteContaBancaria.java
@@ -0,0 +1,20 @@
+package exercicio1oo.classes;
+
+public class TesteContaBancaria {
+    public static void main(String[] args) {
+        ContaBancaria cbx = new ContaBancaria();
+
+        cbx.numeroConta = "182546789271";
+        cbx.titular = "Homer Simpson";
+        cbx.saldo = -2000;
+
+        System.out.println("Olá Homer! Dados da sua conta:");
+        System.out.println("Titular: " + cbx.titular);
+        System.out.println("Nº conta: "+cbx.numeroConta);
+        System.out.println("Saldo: "+cbx.saldo+" $$");
+
+        if (cbx.saldo < 0){
+            System.out.printf("Você está endividado %s !\n\n",cbx.titular);
+        }
+    }
+}
