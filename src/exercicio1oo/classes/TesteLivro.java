1b2e44c411012e23a72057bb6ba14bf5effd9154
Kaio Meireles <dickkaiosilva@gmail.com>
2025-09-06 00:19:27 -0300

    Classe 06 e teste

--- src/exercicio1oo/classes/TesteLivro.java
+++ src/exercicio1oo/classes/TesteLivro.java
@@ -1,7 +1,27 @@
 package exercicio1oo.classes;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class TesteLivro {
     public static void main(String[] args) {
         Livro livro1 = new Livro();
+        livro1.titulo = "Dom Quixote";
+        livro1.autor = "Miguel de Cervantes";
+        livro1.genero = "Romance";
+        livro1.emprestado = true;
+
+        System.out.println("Dados do livro: ");
+        System.out.println("Título: " + livro1.titulo);
+        System.out.println("Autor: " + livro1.autor);
+        System.out.println("Gênero: " + livro1.genero+"\n");
+
+        if (livro1.emprestado == true)
+        {
+            System.out.println("Status: EMPRESTADO");
+        }
+        else
+        {
+            System.out.println("Status: DISPONÍVEL");
+        }
     }
 }
