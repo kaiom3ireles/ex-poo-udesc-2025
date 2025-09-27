7aca85d94186bf614ad184338fc105f9917abcc8
Kaio Meireles <dickkaiosilva@gmail.com>
2025-09-27 00:28:41 -0300

    Ex04 Classe Circulo

--- src/exercicio4oo/classes/Circulo.java
+++ src/exercicio4oo/classes/Circulo.java
@@ -3,6 +3,19 @@ package exercicio4oo.classes;
 public class Circulo {
     private double raio;
 
+    public double calcularDiametro(){
+        double diametro = 2*this.raio;
+        return diametro;
+    }
+    public double calcularArea(){
+        double area = Math.PI*(this.raio*this.raio);
+        return area;
+    }
+    public double calcularPerimetro(){
+        double perimetro = 2*Math.PI*(this.raio*this.raio);
+        return perimetro;
+    }
+
     public double getRaio() {
         return this.raio;
     }
--- src/exercicio4oo/testes/TesteCirculo.java
+++ src/exercicio4oo/testes/TesteCirculo.java
@@ -8,5 +8,8 @@ public class TesteCirculo {
         y.setRaio(9.5);
 
         System.out.printf("O círculo indicado é de raio %.2f\n",y.getRaio());
+
+        System.out.printf("Área: %.2f\nPerímetro: %.2f\nDiâmetro: %.2f\n\n", y.calcularArea(),
+                y.calcularPerimetro(), y.calcularDiametro());
     }
 }
