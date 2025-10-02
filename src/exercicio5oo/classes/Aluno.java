f9a6bd75067a9414f8c875a873dce964a3eca127
Kaio Meireles <dickkaiosilva@gmail.com>
2025-10-01 22:56:32 -0300

    Correção de código e const. parametrizado

--- src/exercicio5oo/classes/Aluno.java
+++ src/exercicio5oo/classes/Aluno.java
@@ -3,16 +3,27 @@ package exercicio5oo.classes;
 public class Aluno {
     //ATRIBUTOS PRIVADOS
     private String matricula, nome;
-    private int idade, nota1, nota2, nota3, nota4;
-
+    private int idade;
+    private double nota1, nota2, nota3, nota4;
     public Aluno(){
         this.matricula = "";
         this.nome = "";
         this.idade = 0;
-        this.nota1 = 0;
-        this.nota2 = 0;
-        this.nota3 = 0;
-        this.nota4 = 0;
+        this.nota1 = 0.0;
+        this.nota2 = 0.0;
+        this.nota3 = 0.0;
+        this.nota4 = 0.0;
+    }
+
+    public Aluno(String matricula, String nome, int idade,  double nota1,
+                 double nota2, double nota3, double nota4){
+        this.matricula = matricula;
+        this.nome = nome;
+        this.idade = idade;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.nota4 = nota4;
     }
 
     //Funçao para calcular média de aluno
@@ -46,16 +57,16 @@ public class Aluno {
     public void setMatricula(String matricula) { this.matricula = matricula; }
     public void setNome(String nome) { this.nome = nome; }
     public void setIdade(int idade) { this.idade = idade; }
-    public void setNota1(int nota1) { this.nota1 = nota1; }
-    public void setNota2(int nota2) { this.nota2 = nota2; }
-    public void setNota3(int nota3) { this.nota3 = nota3; }
-    public void setNota4(int nota4) { this.nota4 = nota4; }
+    public void setNota1(double nota1) { this.nota1 = nota1; }
+    public void setNota2(double nota2) { this.nota2 = nota2; }
+    public void setNota3(double nota3) { this.nota3 = nota3; }
+    public void setNota4(double nota4) { this.nota4 = nota4; }
 
     public String getMatricula() { return this.matricula; }
     public String getNome(){ return this.nome; }
     public int getIdade() { return this.idade; }
-    public int getNota1() { return this.nota1; }
-    public int getNota2() { return this.nota2; }
-    public int getNota3() { return this.nota3; }
-    public int getNota4() { return this.nota4; }
+    public double getNota1() { return this.nota1; }
+    public double getNota2() { return this.nota2; }
+    public double getNota3() { return this.nota3; }
+    public double getNota4() { return this.nota4; }
 }
--- src/exercicio5oo/testes/TesteAluno.java
+++ src/exercicio5oo/testes/TesteAluno.java
@@ -6,15 +6,18 @@ public class TesteAluno {
     public static void main(String[] args) {
 
         Aluno kaio = new Aluno();
+        Aluno chaves = new Aluno("888888888", "Chaves del ocho",
+                                 8, 1.0, 1.5, 4.2, 5.0);
         kaio.setMatricula("123456789");
         kaio.setNome("Kaio Meireles");
         kaio.setIdade(21);
-        kaio.setNota1(7);
+        kaio.setNota1(8.5);
         kaio.setNota2(7);
-        kaio.setNota3(7);
-        kaio.setNota4(7);
+        kaio.setNota3(6.5);
+        kaio.setNota4(9.2);
 
         System.out.println(kaio);
+        System.out.println(chaves);
 
     }
 }
