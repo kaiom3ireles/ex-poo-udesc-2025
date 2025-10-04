f81c21dc9a22171d9b4dd5ac83acf6acd662930a
Kaio Meireles <dickkaiosilva@gmail.com>
2025-10-03 23:40:19 -0300

    Ex5 Classe Circulo

--- src/exercicio5oo/classes/Circulo.java
+++ src/exercicio5oo/classes/Circulo.java
@@ -3,6 +3,13 @@ package exercicio5oo.classes;
 public class Circulo {
     private double raio;
 
+    public Circulo(){
+        this.raio = 0.0;
+    }
+
+    public Circulo(double raio){
+        this.raio= raio;
+    }
     public double calcularDiametro(){
         double diametro = 2*this.raio;
         return diametro;
@@ -14,6 +21,14 @@ public class Circulo {
     public double calcularPerimetro(){
         double perimetro = 2*Math.PI*(this.raio*this.raio);
         return perimetro;
+    }
+
+    public String toString(){
+        return "Dados do retângulo: "
+                + "\nRaio: " + this.raio
+                + "\nDiametro: " + this.calcularDiametro()
+                + "\nArea: " + this.calcularArea()
+                + "\nPerimetro: " + this.calcularPerimetro();
     }
 
     public double getRaio() {
--- src/exercicio5oo/testes/TesteCirculo.java
+++ src/exercicio5oo/testes/TesteCirculo.java
@@ -4,12 +4,15 @@ import exercicio5oo.classes.Circulo;
 
 public class TesteCirculo {
     public static void main(String[] args) {
-        Circulo y = new Circulo();
-        y.setRaio(9.5);
+        Circulo a = new Circulo();
+        Circulo b = new Circulo(2.5);
+        a.setRaio(9.5);
 
-        System.out.printf("O círculo indicado é de raio %.2f\n",y.getRaio());
+        System.out.printf("O círculo indicado é de raio %.2f\n",a.getRaio());
 
-        System.out.printf("Área: %.2f\nPerímetro: %.2f\nDiâmetro: %.2f\n\n", y.calcularArea(),
-                y.calcularPerimetro(), y.calcularDiametro());
+        System.out.printf("Área: %.2f\nPerímetro: %.2f\nDiâmetro: %.2f\n\n", a.calcularArea(),
+                a.calcularPerimetro(), a.calcularDiametro());
+        System.out.println("--------------------------");
+        System.out.println(b);
     }
 }
